34d3f8fc3b8ae0838a494646cc5c3f067f799542
Mohammed Razak <mohammedrazak2001@gmail.com>
2025-04-06 23:56:19 +0530

    find packages using ament_prefix_path env hook

--- src/rust_nodes/src/lsp_server.rs
+++ src/rust_nodes/src/lsp_server.rs
@@ -1,9 +1,19 @@
+use glob::glob;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
 
+use std::collections::HashMap;
+use std::env;
+
+fn get_prefix_dirs() -> Vec<String> {
+    let amnt_prefix = env::var("AMENT_PREFIX_PATH").unwrap_or_default();
+    amnt_prefix.split(':').map(|s| s.to_string()).collect()
+}
+
 struct Backend {
     client: Client,
+    definitions: HashMap<String, Location>,
 }
 
 #[tower_lsp::async_trait]
@@ -29,7 +39,7 @@ impl LanguageServer for Backend {
 
     async fn initialized(&self, _: InitializedParams) {
         self.client
-            .log_message(MessageType::INFO, "LSP server initialized!")
+            .log_message(MessageType::INFO, "LSP server initialized :)")
             .await;
     }
 
@@ -52,18 +62,22 @@ impl LanguageServer for Backend {
             )
             .await;
 
-        // Dummy example: Return a fixed location for demonstration purposes.
-        let def_location = Location {
-            uri: "file:///home/razak/ros2_ws/src/rust_nodes/launch/include_definition.py"
-                .parse()
-                .unwrap(),
-            range: Range {
-                start: Position { line: 9, character: 4 },
-                end: Position { line: 9, character: 20 },
-            },
-        };
 
-        Ok(Some(GotoDefinitionResponse::Scalar(def_location)))
+        eprintln!("definitions size: {}", self.definitions.len());
+
+        let token = "publisher".to_string();
+        
+        if let Some(location) = self.definitions.get(&token) {
+            Ok(Some(GotoDefinitionResponse::Scalar(location.clone())))
+        } else {
+            self.client
+                .log_message(
+                    MessageType::WARNING,
+                    format!("No definition found for token: {}", token),
+                )
+                .await;
+            Ok(None)
+        }
     }
 
     // Provide basic code completion suggestions.
@@ -93,11 +107,82 @@ impl LanguageServer for Backend {
     }
 }
 
+fn construct_pattern(prefix: &str) -> String {
+    format!("{}/share/**/rust/**/*.rs", prefix)
+}
+
+fn get_rust_nodes_path(prefixes: Vec<String>) -> Vec<String> {
+    let mut rust_nodes_paths: Vec<String> = Vec::new();
+    for prefix in prefixes {
+        if(prefix.contains("rust_nodes")) {
+            rust_nodes_paths.push(prefix.to_string());
+        }
+    }
+    return rust_nodes_paths;
+}
+
+fn scan_workspace_for_definitions() -> HashMap<String, Location> {
+    let mut definitions = HashMap::new();
+
+    eprintln!("Scanning workspace for definitions...");
+    let prefixes: Vec<String> = get_prefix_dirs();
+    // for now lets look only for rust_nodes/rust_nodes_bringup
+    let rust_node_definitions: Vec<String> = get_rust_nodes_path(prefixes);
+    for path in rust_node_definitions {
+        let pattern = construct_pattern(&path);
+        eprintln!("Looking for Rust nodes in: {}", pattern);
+        // glob to scan workspace 
+        for entry in glob(&pattern).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => {
+                    eprintln!();
+                    eprintln!("Found Rust node: {:?}", path.display());
+                    
+                    // For demonstration, use file stem as token.
+                    if let Some(file_stem) = path.file_stem().and_then(|s| s.to_str()) {
+                        let location = Location {
+                            uri: format!("file://{}", path.display()).parse().unwrap(),
+                            range: Range {
+                                start: Position { line: 0, character: 0 },
+                                end: Position { line: 0, character: 0 },
+                            },
+                        };
+                        definitions.insert(file_stem.to_string(), location);
+                        eprintln!("Indexed token '{}' -> {}", file_stem, path.display());
+                        eprintln!();
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Error reading path: {:?}", e);
+                }
+            }
+        }
+    }
+    definitions
+}
+
 #[tokio::main]
 async fn main() {
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+
+    let prefixes: Vec<String> = get_prefix_dirs();
+    for p in &prefixes {
+        eprintln!("Found prefix directory: {}", p);
+    }
+
+    let definitions_map = scan_workspace_for_definitions();
+    
+    eprintln!("Total definitions indexed: {}", definitions_map.len());
+    for (token, location) in &definitions_map {
+        eprintln!("Token: {}, Location: {:?}", token, location);
+    }
+    
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        definitions: definitions_map,
+    });
+
     Server::new(stdin, stdout, socket).serve(service).await;
 }
